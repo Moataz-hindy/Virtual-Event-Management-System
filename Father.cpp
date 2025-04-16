f02bca0ad578b1f4d422a5d0207463dfa64e04f9
Mohamed Elshabouri <77917654+WhipShip@users.noreply.github.com>
2025-04-17 00:56:27 +0200

    Update Father.cpp

--- Father.cpp
+++ Father.cpp
@@ -1,89 +1,83 @@
 #include <iostream>
 #include <fstream>
 #include <unordered_set>
-#inlcude <string>
+#include <string>
 
 using namespace std;
 
-void login(){
+void login() {
     string username, password;
     bool loginSuccess = false;
-    
+
     cout << "Enter username: ";
     cin >> username;
     cout << "Enter password: ";
     cin >> password;
-    
-    fstream users("loginDataBase.txt");
-    
+
+    ifstream users("loginDataBase.txt");
     if (!users.is_open()) {
         cout << "Error: Unable to open database file!" << endl;
-        return ;
+        return;
     }
-    
+
     string fileUsername, filePassword;
-    
-    while (getline(users, fileUsername)) {
-        if (getline(users, filePassword)) {  
-            if (fileUsername == username && filePassword == password) {
-                loginSuccess = true;
-                break;
-            }
+    while (users >> fileUsername >> filePassword) {
+        if (fileUsername == username && filePassword == password) {
+            loginSuccess = true;
+            break;
         }
     }
 
     users.close();
-    
+
     if (loginSuccess) {
-        cout << "Welcome, " << username << endl;
+        cout << "Welcome, " << username << "!" << endl;
     } else {
         cout << "Invalid username or password!" << endl;
-        login();
+        login(); // optionally loop back for retry
     }
-    
 }
 
+void signup(unordered_set<string>& usernames) {
+    string username, password;
 
-void signup(unordered_set<string> usernames){
+    cout << "Enter username: ";
+    cin >> username;
 
-        string username, password;
-
-        fstream users("loginDataBase.txt", ios::app);
-
-        if (!users.is_open()) {
-            cout << "Error: Unable to open database file!" << endl;
-            return ;
-        }
-
-        cout << "Enter username: ";
-        cin >> username;
-        cout << "Enter password: ";
-        cin >> password;
-
-        if(usernames.count(username)){
-            cout << "Username is taken!" << endl;
-            signup(usernames);
-        }else{
-            users << username << " " << password << "\n";
-        }
-
-        users.close();
+    if (usernames.count(username)) {
+        cout << "Username is taken!" << endl;
+        signup(usernames);
+        return;
     }
 
+    cout << "Enter password: ";
+    cin >> password;
 
-void start_menu(unordered_set<string> usernames){
+    ofstream users("loginDataBase.txt", ios::app);
+    if (!users.is_open()) {
+        cout << "Error: Unable to open database file!" << endl;
+        return;
+    }
+
+    users << username << " " << password << "\n";
+    users.close();
+    usernames.insert(username);
+
+    cout << "Signup successful!" << endl;
+}
+
+void start_menu(unordered_set<string>& usernames) {
     int choice;
-    cout << "Please choose one of these options:" << endl;
+    cout << "\nPlease choose one of these options:" << endl;
     cout << "1) Login" << endl;
     cout << "2) Signup" << endl;
     cout << "3) Exit" << endl;
     cout << "Enter a number: ";
     cin >> choice;
 
-    switch (choice){
-        case 1 :
+    switch (choice) {
+        case 1:
             login();
-            //start_menu(usernames);
             break;
         case 2:
             signup(usernames);
@@ -91,153 +85,98 @@ void start_menu(unordered_set<string> usernames){
             break;
         case 3:
             exit(0);
-            break;
         default:
-            cout << "Invalid choice, please choose 1, 2 or 3" << endl;
-            start_menu(usernames); 
-            break;
+            cout << "Invalid choice, please choose 1, 2 or 3." << endl;
+            start_menu(usernames);
     }
 }
 
-
-void setup(){
-    
+void setup() {
     unordered_set<string> usernames;
-    string userName;
-    fstream users("loginDataBase.txt");
-    while(users >> userName){
+    string userName, dummy;
+    ifstream users("loginDataBase.txt");
+
+    while (users >> userName >> dummy) {
         usernames.insert(userName);
-        users >> userName;
     }
     users.close();
 
     cout << "=========================" << endl;
-    cout << "=========================" << endl;
     cout << "=========WELCOME=========" << endl;
-    cout << "=========================" << endl;
     cout << "=========================" << endl;
 
     start_menu(usernames);
-
 }
 
-class Event{
+class Event {
 private:
-    string event_name;
-    string description;
-    string platform;
-    string date;
-    string time;
+    string event_name, description, platform, date, time, type;
     int capacity;
-    string type;
+
 public:
-   Event() {} // Default constructor used in order to make the classe can be used in arrays
-   Event(string n, string desc, string d, string t, string p, int c, string typ){
-   event_name = n;
-   description = desc;
-   platform = p;
-   date = d;
-   time = t;
-   capacity = c ;
-   type = typ;
-   }
-   void displayDetails() const {
-        cout << "Event: " << event_name << " (" << type << ")"<<endl;
+    Event() {} // default constructor
+    Event(string n, string desc, string d, string t, string p, int c, string typ)
+        : event_name(n), description(desc), date(d), time(t), platform(p), capacity(c), type(typ) {}
+
+    void displayDetails() const {
+        cout << "Event: " << event_name << " (" << type << ")" << endl;
         cout << "Date: " << date << " at " << time << endl;
         cout << "Platform: " << platform << endl;
-        cout << "Description: " << description <<endl;
-        cout << "Capacity: " << capacity  << endl;
+        cout << "Description: " << description << endl;
+        cout << "Capacity: " << capacity << endl;
     }
-    string getName() const {
-        return event_name ;
-        }
-    string getDescription() const {
-        return description;
-         }
-    string getDate() const {
-        return date;
-        }
-    string getTime() const {
-        return time;
-         }
-    string getPlatform() const {
-        return platform;
-         }
-    int getCapacity() const {
-        return capacity;
-        }
-    string getType() const {
-        return type;
-        }
 
+    string getName() const { return event_name; }
+    string getDescription() const { return description; }
+    string getDate() const { return date; }
+    string getTime() const { return time; }
+    string getPlatform() const { return platform; }
+    int getCapacity() const { return capacity; }
+    string getType() const { return type; }
 };
-class Attendee{
+
+class Attendee {
+private:
+    string name, email, affiliation;
+
+public:
+    Attendee() {}
+    Attendee(string n, string e, string a) : name(n), email(e), affiliation(a) {}
+
+    void setEmail(string e) { email = e; }
+    string getEmail() const { return email; }
+
+    void setName(string n) { name = n; }
+    string getName() const { return name; }
+
+    void setAffiliation(string a) { affiliation = a; }
+    string getAffiliation() const { return affiliation; }
+};
+
+class Feedback {
 private:
     string name;
-    string email;
-    string affiliation;
+    int rating;
+    string review;
+
 public:
-    Attendee() {}// Default constructor used in order to make the classe can be used in arrays
-    Attendee(string n, string e, string a){
-    name = n;
-    email = e;
-    affiliation = a;
+    Feedback() {}
+    Feedback(string n, int ra, string re)
+        : name(n), rating(ra), review(re) {
+        if (rating < 0) rating = 0;
+        if (rating > 5) rating = 5;
     }
-    void setEmail(string e){
-    email = e;
+
+    void display() const {
+        cout << name << " rated " << rating << " out of 5" << endl;
+        cout << "Review: " << review << endl;
     }
-    string getEmail ()const{
-    return email;
-    }
-    void setName(string n){
-       name = n;
-    }
-    string getName ()const{
-    return name ;
-    }
-        void setAffiliation(string a){
-       affiliation = a;
-    }
-    string getAffiliation()const{
-    return affiliation ;
-    }
- // void display(){}
-};
-class Feedback{
-private:
-    string name;
-    int ratings;
-    string reviews;
-public:
-    Feedback() {}// Default constructor used in order to make the classe can be used in arrays
-    Feedback(string n,int ra, string re){
-    name = n;
-    ratings = ra;
-    reviews = re;
-// To ensure ratings are within the range
-        if (ratings < 0) {
-            ratings = 0;
-     }
-        if (ratings > 5){
-                ratings = 5;
-     }
-}
- void display()  {
-        cout <<name<<" rated "<< ratings <<"out of 5"<<endl;
-        cout << "Review: " << reviews <<endl;
-    }
-     int getRating() const {
-          return rating; 
-          }
-    string getReview() const {
-         return review; 
-         }
+
+    int getRating() const { return rating; }
+    string getReview() const { return review; }
 };
 
-
-int main(){
-
+int main() {
     setup();
-
     return 0;
 }
