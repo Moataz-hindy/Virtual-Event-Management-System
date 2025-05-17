c5f3742923d2ff5e87c7936c85ec85fc7d133011
Mohamed Elshabouri <77917654+WhipShip@users.noreply.github.com>
2025-05-18 01:43:24 +0300

    Update events.h

--- Main/events.h
+++ Main/events.h
@@ -16,55 +16,62 @@
 #include <cctype> // used for character handling
 #include <chrono> // used for time management
 #include <algorithm>
+#include <chrono>
+#include <thread>
 
 using namespace std; // using a standard namespace
-
-extern unordered_set<string> usernames; // a global username registry for quick lookups
 
 enum eventType {
     Conference_, Workshop_, Webinar_
 };
 
+enum notificationType {
+    Event_postponement,Event_cancellation, Event_registration, Event_feedback
+};
+
+class User;
+class Observer;
+
 // an abstract base class called event
 class Event {
-private:
-    string event_name, description, platform, date, time; // private event attributes with event's details
+protected:
+    string event_name, creator_name, description, platform, date, time; // private event attributes with event's details
     int capacity; // stores max attendees
-    set <string> registrars; // registered users
+    set <User*> registrars; // registered users
     bool full; // the max capacity flag
+    vector<Observer*> observers_; // an observer pointer to notify observers
+    User* creator_user; // a pointer to the creator user
 
 public:
     Event(); // a default constructor
-    Event(string n, string desc, string p, string d, string t, int c, int f); // a parametrised constructor
+    Event(string n, User* creator, string desc, string p, string d, string t, int c, int f); // a parametrised constructor
     virtual ~Event() = default; // a virtual destructor
 
     // pure virtual functions
     virtual void displayDetails() const = 0; // shows the event's details
     // Factory Method: Implements the Factory Design Pattern
-    static Event* eventBuilder(eventType Type);
-    virtual void saveToFile(const string& username) = 0; // saves the event to file
+    static Event* eventBuilder(eventType Type, User* creator);
+    virtual void saveToFile(ofstream& file) = 0; // saves the event to file
     bool isFull(); // checks if the event has reached max capacity
-    static bool isRegistered(const string& username, Event* e); // checks if a username is registered for a specific event
-    set<string> getRegistrars(); // gets registered users in an event
-    bool addRegistrar(const string& username); // adds the user to registrars
-    void saveRegistrar(const string& username);
-    static void schedule_event(string const& logged_user, Event* event); // a function that allows the user to create + schedule an event
-    static void meeting_postponement(const string& username); // a function to handle rescheduling of existing events
-    static void meeting_cancellation(const string& username); // a function to cancel any scheduled events
-    static void search_by_name(); // searches for events by name
-    static void search_by_date(); // searches for events by date
-    static void search_by_type(); // searches for events by type
-    static void register_event(const string& username); // a function for the user to register into an event
-    static void loadEventsData();
+    set<User*> getRegistrars(); // gets registered users in an event
+    bool addRegistrar(User* user); // adds the user to registrars
 
     // getter functions to get the event's details
     string getName() const;
+    User* getCreator() const;
     string getDescription() const;
     string getDate() const;
     string getTime() const;
     string getPlatform() const;
     int getCapacity() const;
     virtual string getType() const = 0; // gets the event type
+    void setDate(string newDate);
+    void setTime(string newTime);
+
+    //Observer Methods
+    void attach(Observer* observer);
+    void detach(Observer* observer);
+    void notify(notificationType);
 
     bool operator<(const Event& other) const; // an operator overload
 };
@@ -75,14 +82,14 @@ private:
     int duration; // a private attribute that checks the duration of a conference
 public:
     Conference(); // a default constructor
-    Conference(string n, string desc, string p, string d, string t, int c, int dur); // a parametrised constructor
+    Conference(string n, User* creator, string desc, string p, string d, string t, int c, int dur); // a parametrised constructor
     ~Conference() override = default; // a destructor
 
     // overridden virtual functions
     void displayDetails() const override;
     // Static Factory Method: Part of Factory Design Pattern
-    static Event* create_event();
-    void saveToFile(const string& username) override;
+    static Event* create_event(User* creator);
+    void saveToFile(ofstream& file) override;
     static Event* loadFromFile(const string& line); // static type load method/function
 
     // getter functions
@@ -96,14 +103,14 @@ private:
     string host; // a private attribute that stores the host
 public:
     Webinar(); // a default constructor
-    Webinar(string n, string desc, string p, string d, string t, int c, string h); // a parametrised constructor
+    Webinar(string n, User* creator, string desc, string p, string d, string t, int c, string h); // a parametrised constructor
     ~Webinar() override = default; // a destructor
 
     // overridden virtual functions
     void displayDetails() const override;
     // Static Factory Method: Part of Factory Design Pattern
-    static Event* create_event();
-    void saveToFile(const string& username) override;
+    static Event* create_event(User* creator);
+    void saveToFile(ofstream& file) override;
     static Event* loadFromFile(const string& line); // a static type load method/function
 
     // getter functions
@@ -117,20 +124,22 @@ private:
     string instructor; // a private attribute that stores the workshop's instructor
 public:
     Workshop(); // a default constructor
-    Workshop(string n, string desc, string p, string d, string t, int c, string i); // a parametrised constructor
+    Workshop(string n, User* creator, string desc, string p, string d, string t, int c, string i); // a parametrised constructor
     ~Workshop() override = default; // a destructor
 
     // overridden virtual functions
     void displayDetails() const override;
     // Static Factory Method: Part of Factory Design Pattern
-    static Event* create_event();
-    void saveToFile(const string& username) override;
+    static Event* create_event(User* creator);
+    void saveToFile(ofstream& file) override;
     static Event* loadFromFile(const string& line); // static type load method/function
 
     // getter functions
     string getType() const override;
     string getInstructor() const;
 };
+
+extern set <Event*> allEvents; // declares a global set of event pointers
 
 class Observer{
     public:
@@ -147,15 +156,7 @@ public:
     User(string u, string p, string e, string a = ""); // a parametrised constructor
     virtual ~User() = default; // a virtual destructor
 
-    User User_Factory(string const& username, string const& password, string const& email, string const& affiliation); // a factory pattern implementation for creating new User objects
     virtual void update(const string& eventName, const string& message) override;
-
-    static void signup(); // a function that handles a new user's registration
-    static string login(); // a function that handles user authentication + returns logged-in username
-    static set<Event*> loadEventsForUser(const string& username); // a function that loads all events registered by a specific user
-    static void loadLoginData();
-    static void viewUserEvents(const string& logged_user); // a function that displays all events associated with a user
-    static set<Event*> get_registered_events(const string& username); // gets all events a user is registered for
 
     void setUsername(string u); // sets the username
     string getUsername() const; // gets the username
@@ -169,10 +170,20 @@ public:
     void setAffiliation(string a); // set the affiliation
     string getAffiliation() const; // gets the affiliation
 
+    set<Event*> get_scheduled_events(); // a function that loads all events scheduled by user
+    set<Event*> get_registered_events(); // a function that loads all events registered by user
+    void schedule_event(); // a function that allows the user to create + schedule an event
+    void register_event(); // a function for the user to register into an event
+    void viewUserEvents(); // a function that displays all events associated with a user
+    bool isRegistered(Event* e); // checks if a user is registered for a specific event
+    void postpone_meeting(); // a function to handle rescheduling of existing events
+    void cancel_meeting(); // a function to cancel any scheduled events
+
+
     bool operator<(const User& other) const; // a comparison operator for when we sort/order users
 };
 
-extern set <Event*> allEvents; // declares a global set of event pointers
+extern set <User*> allUsers; // declares a global set of event pointers
 
 // a class that stores + manages feedback for events
 class Feedback {
@@ -213,7 +224,7 @@ public:
     bool collectFeedbackInteractive(const vector<string>& potentialAspects); // interactive feedback collection (returns true if submitted)
     void saveToFile() const; // saves feedback to the file
     bool isSubmittedBeforeEvent() const; // checks if the feedback was submitted before the event took place
-    static void review_feedbacks(const string& username); // a function that displays feedback for user's events
+    static void review_feedbacks(User* user); // a function that displays feedback for user's events
 
 private:
     static string getCurrentTimestamp(); // gets the current time stamp
@@ -231,11 +242,22 @@ class Menu {
 public:
     void setup(); // a function that initializes system data + loads saved state
     void start_menu(); // a function that displays the initial starting menu
-    void main_menu(string const& logged_user); // the main interface after the user successfully logs in
-    void events_menu(string const& logged_user); // a function that displays events related operations
-    void search_(); // the main interface for the searching functions
-    void feedback_menu(const string& logged_user); // an interface for feedback management
+    void main_menu(User* user); // the main interface after the user successfully logs in
+    void search_menu(); // the main interface for the searching functions
+    void feedback_menu(User* user); // an interface for feedback management
 };
+
+// Global functions
+void signup(); // a function that handles a new user's sign-up
+User* login(); // a function that handles user authentication + returns logged-in user
+void loadEventsData();
+void loadUsersData();
+void saveEventsToFile();
+void saveUsersToFile();
+void search_by_name(); // searches for events by name
+void search_by_date(); // searches for events by date
+void search_by_type(); // searches for events by type
+void loadingAnimation(int seconds);
 
 template <typename T> void printMultiset(const set<T>& mset); // a utility template to print any set's contents
 
